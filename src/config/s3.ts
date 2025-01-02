63c6749ffa0edd1d75891da8e16491628159fbee
Akash <AwesomeAkash47@gmail.com>
2025-01-02 19:31:59 +0530

    changed to node:alpine in docker

--- src/config/s3.ts
+++ src/config/s3.ts
@@ -3,12 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const accessKeyId = process.env.AWS_S3_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_S3_SECRET_ACCESS_KEY;
-const region = process.env.AWS_REGION;
-
-console.log(accessKeyId);
-
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID, // Replace with your AWS Access Key
   secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY, // Replace with your AWS Secret Key
